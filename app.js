6a903eb57ef49c61cad85482e7c7aaa5da97e95a
riteshnath <nathritesh98@gmail.com>
2023-09-11 12:44:36 +0530

    Initial Project setup- creating routes and controllers file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+//MIDDLEWARES
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    results: tours.length,
+    tours,
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    tour,
+  });
+};
+
+exports.createTour = (req, res) => {
+  console.log(req.body);
+};
+
+exports.updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    tour,
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  res.status(204).json({
+    data: null,
+  });
+};
