c71f4fac8cb3b51463c2b6e3ab8d95f9ce5a6dcc
riteshnath <nathritesh98@gmail.com>
2023-09-12 17:11:43 +0530

    Configuring remote mongodb atlas

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 //MIDDLEWARES
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const PORT = 3000;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log("Connected!"));
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`App running on port no: ${PORT}`);
 });
